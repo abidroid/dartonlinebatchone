10f6d01730785dfdb459da2e690c030afebacdf5
Abid <abidroid1@gmail.com>
2025-04-23 21:52:54 +0500

    simple calculator

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,35 +3,40 @@ import 'dart:io';
 
 void main()
 {
-  int monthNumber;
+  int n1, n2, choice;
+  stdout.write('Enter first number : ');
+  n1 = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter second number : ');
+  n2 = int.parse(stdin.readLineSync()!);
+  String message = '''
+Select Operation
+1. Add
+2. Sub
+3. Mul
+4. Div
+Your choice ? ''';
 
-  stdout.write('Enter month number : ');
-  monthNumber = int.parse( stdin.readLineSync()! );
+  stdout.write(message);
+  choice = int.parse(stdin.readLineSync()!);
 
-  if( monthNumber == 1 ){
-    print('January');
+  if( choice == 1){
+    print('Sum = ${n1+n2}');
   }
 
-  if( monthNumber == 2 ){
-    print('February');
+  if( choice == 2 ){
+    print('Difference = ${n1-n2}');
   }
 
-  if( monthNumber == 3 ) {
-    print('March');
+  if( choice == 3){
+    print('Product = ${n1*n2}');
   }
 
-  // write yourself
-
-  if( monthNumber == 12 ) {
-    print('December');
+  if( choice == 4){
+    print('Quotient = ${n1 / n2}');
   }
 
-  if( monthNumber > 12 ) {
-    print('Invalid month number');
-  }
-
-  if( monthNumber <= 0) {
-    print('Invalid month number');
+  if( choice > 5 ){
+    print('Invalid operation code');
   }
 }
 
