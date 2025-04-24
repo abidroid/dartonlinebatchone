52a60461c579cda67350fb3dd88c45e516d1a5bc
Abid <abidroid1@gmail.com>
2025-04-24 21:16:02 +0500

    multiple if

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,41 +3,18 @@ import 'dart:io';
 
 void main()
 {
-  int n1, n2, choice;
+  int n1, n2;
   stdout.write('Enter first number : ');
-  n1 = int.parse(stdin.readLineSync()!);
+  n1 = int.parse( stdin.readLineSync()!);
+
   stdout.write('Enter second number : ');
-  n2 = int.parse(stdin.readLineSync()!);
-  String message = '''
-Select Operation
-1. Add
-2. Sub
-3. Mul
-4. Div
-Your choice ? ''';
+  n2 = int.parse( stdin.readLineSync()!);
 
-  stdout.write(message);
-  choice = int.parse(stdin.readLineSync()!);
+  if( n1 > n2 ) print('Large number is $n1');
 
-  if( choice == 1){
-    print('Sum = ${n1+n2}');
-  }
+  if( n2 > n1 ) print('Large number is $n2');
 
-  if( choice == 2 ){
-    print('Difference = ${n1-n2}');
-  }
-
-  if( choice == 3){
-    print('Product = ${n1*n2}');
-  }
-
-  if( choice == 4){
-    print('Quotient = ${n1 / n2}');
-  }
-
-  if( choice > 5 ){
-    print('Invalid operation code');
-  }
+  if( n1 == n2 ) print( 'Numbers are same');
 }
 
 
