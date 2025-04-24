88519d5192b8519e2b7ecc4cc1c48349971b9d19
Abid <abidroid1@gmail.com>
2025-04-24 21:33:10 +0500

    simple calc with ifelseifelse ladder

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,24 +3,36 @@ import 'dart:io';
 
 void main()
 {
-  int n1, n2;
+  int n1, n2, choice;
   stdout.write('Enter first number : ');
-  n1 = int.parse( stdin.readLineSync()!);
-
+  n1 = int.parse(stdin.readLineSync()!);
   stdout.write('Enter second number : ');
-  n2 = int.parse( stdin.readLineSync()!);
+  n2 = int.parse(stdin.readLineSync()!);
+  String message = '''
+ Select Operation
+ 1. Add
+ 2. Sub
+ 3. Mul
+ 4. Div
+ Your choice ? ''';
 
-  if( n1 > n2 )
-  {
-    print('Large number is $n1');
+  stdout.write(message);
+  choice = int.parse(stdin.readLineSync()!);
+
+  if( choice == 1){
+    print('Sum = ${n1+n2}');
   }
-  else if( n2 > n1 )
-  {
-    print('Large number is $n2');
+  else if( choice == 2 ){
+    print('Difference = ${n1-n2}');
   }
-  else
-  {
-    print( 'Numbers are same');
+  else if( choice == 3){
+    print('Product = ${n1*n2}');
+  }
+  else if( choice == 4){
+    print('Quotient = ${n1 / n2}');
+  }
+  else {
+    print('Invalid operation code');
   }
 }
 
