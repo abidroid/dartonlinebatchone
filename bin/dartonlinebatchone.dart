e03323bc3dc414f06e7815f86f596c473f25abc7
Abid <abidroid1@gmail.com>
2025-04-28 21:20:21 +0500

    ?:

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,20 +3,26 @@ import 'dart:io';
 
 void main()
 {
-  String result = '';
+  int n1, n2, large;
 
-  stdout.write('Enter a number : ');
-  int number = int.parse( stdin.readLineSync()!);
+  stdout.write('Enter first number : ');
+  n1 = int.parse( stdin.readLineSync()!);
 
-  result = ( number % 2 == 0 ) ? "Even" : "Odd";
+  stdout.write('Enter second number : ');
+  n2 = int.parse( stdin.readLineSync()!);
 
-  // if( number % 2 == 0 ){
-  //   result = "Even";
-  // }else{
-  //   result = "Odd";
+  large = (n1 > n2) ? n1 : n2;
+
+  // if( n1 > n2 )
+  // {
+  //   large = n1;
+  // }
+  // else
+  // {
+  //   large = n2;
   // }
 
-  print('$number is $result');
+  print('Large number is $large');
 }
 
 
