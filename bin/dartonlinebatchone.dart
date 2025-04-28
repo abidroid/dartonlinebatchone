2941491a1f41085a8cd8cb9a0ed3bd23df40da2e
Abid <abidroid1@gmail.com>
2025-04-28 21:13:34 +0500

    ?:

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -1,58 +1,22 @@
 
 import 'dart:io';
-import 'dart:vmservice_io';
 
 void main()
 {
-  int n1, n2;
-  String choice;
-  stdout.write('Enter first number : ');
-  n1 = int.parse(stdin.readLineSync()!);
-  stdout.write('Enter second number : ');
-  n2 = int.parse(stdin.readLineSync()!);
-  String message = '''
- Select Operation
- +. Add
- -. Sub
- *. Mul
- /. Div
- Your choice ? ''';
+  String result = '';
 
-  stdout.write(message);
-  choice = stdin.readLineSync()!;
+  stdout.write('Enter a number : ');
+  int number = int.parse( stdin.readLineSync()!);
 
-  switch( choice )
-  {
-    case "+":
-      print('Sum = ${n1+n2}');
-    case "-":
-      print('Difference = ${n1-n2}');
-    case "*":
-      print('Product = ${n1*n2}');
-    case "/":
-      print('Quotient = ${n1/n2}');
-    default:
-      print("Invalid operation code");
-  }
+  result = ( number % 2 == 0 ) ? "Even" : "Odd";
 
-
-
-
-  // if( choice == 1){
-  //   print('Sum = ${n1+n2}');
+  // if( number % 2 == 0 ){
+  //   result = "Even";
+  // }else{
+  //   result = "Odd";
   // }
-  // else if( choice == 2 ){
-  //   print('Difference = ${n1-n2}');
-  // }
-  // else if( choice == 3){
-  //   print('Product = ${n1*n2}');
-  // }
-  // else if( choice == 4){
-  //   print('Quotient = ${n1 / n2}');
-  // }
-  // else {
-  //   print('Invalid operation code');
-  // }
+
+  print('$number is $result');
 }
 
 
