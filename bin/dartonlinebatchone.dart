d82d76a94de25dfe46565e67d3bafa9548ac6ad2
Abid <abidroid1@gmail.com>
2025-04-30 22:04:21 +0500

    and operator

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,34 +3,28 @@ import 'dart:io';
 
 void main()
 {
-  int n1, n2, n3, large;
+  double per;
+  String grade = '';
+  stdout.write('Enter Percentage : ');
+  per = double.parse(stdin.readLineSync()!);
 
-  stdout.write('Enter first number : ');
-  n1 = int.parse( stdin.readLineSync()!);
-
-  stdout.write('Enter second number : ');
-  n2 = int.parse( stdin.readLineSync()!);
-
-  stdout.write('Enter third number : ');
-  n3 = int.parse( stdin.readLineSync()!);
-
-  if( n1 == n2 && n2 == n3){
-    print('All numbers are equal');
+  if( per >= 80 ){
+    grade = 'A+';
+  }
+  else if( per >= 70  ){
+    grade = 'A';
+  }
+  else if( per >= 60 ){
+    grade = 'B';
+  }
+  else if( per >= 40  ){
+    grade = 'C';
+  }
+  else{
+    grade = 'Fail';
   }
 
-  if( n1 > n2 && n1 > n3 ){
-    print('Largest number is $n1');
-  }
-
-  if( n2 > n1 && n2 > n3){
-    print("Largest number is $n2");
-  }
-
-  if( n3 > n1 && n3 > n2 ){
-    print('Largest number is $n3');
-  }
-
-
+  print("Your grade is $grade");
 
 }
 
