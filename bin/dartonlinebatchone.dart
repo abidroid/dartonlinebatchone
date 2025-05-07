55899e3568c6a7798599f1d123b346908c207ff4
Abid <abidroid1@gmail.com>
2025-05-07 21:29:27 +0500

    while loop sentinel conditoin

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,15 +3,25 @@ import 'dart:io';
 
 void main()
 {
+  int num1, num2;
+  String choice = "yes"; // majboori
 
-  int i = 1;
-
-  while( i < 11 )
+  while( choice == "yes") // sentinel condition
   {
-    print('DART $i');
-    i++;
+
+    stdout.write('Enter first number : ');
+    num1 = int.parse( stdin.readLineSync()!);
+
+    stdout.write('Enter second number : ');
+    num2 = int.parse( stdin.readLineSync()!);
+
+    print('Sum = ${num1 + num2}');
+
+    stdout.write('Do you want more additions(yes/no) ? ');
+    choice = stdin.readLineSync()!;
   }
 
+  print('Thank You for using our app. Come back soon');
 }
 
 
