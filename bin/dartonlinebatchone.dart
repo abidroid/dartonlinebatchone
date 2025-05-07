8ec51decabdd1b483693ac4fb1f950a94210bb13
Abid <abidroid1@gmail.com>
2025-05-07 21:52:43 +0500

    nested loops

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -3,24 +3,17 @@ import 'dart:io';
 
 void main()
 {
-  int num1, num2;
-  String choice;
+  int minute, second;
 
-  do
+  for( minute = 0; minute < 60; minute++)
   {
-    stdout.write('Enter first number : ');
-    num1 = int.parse( stdin.readLineSync()!);
+    for( second = 0; second < 60; second++ )
+    {
+      print('$minute : $second');
+    }
 
-    stdout.write('Enter second number : ');
-    num2 = int.parse( stdin.readLineSync()!);
-
-    print('Sum = ${num1 + num2}');
-
-    stdout.write('Do you want more additions(yes/no) ? ');
-    choice = stdin.readLineSync()!;
-  }while( choice == "yes"); // sentinel condition
-
-  print('Thank You for using our app. Come back soon');
+    print('$minute minute gone');
+  }
 }
 
 
