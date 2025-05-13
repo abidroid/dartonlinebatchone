97478275d3d90524acac9fc58b0f9601d98d656c
Abid <abidroid1@gmail.com>
2025-05-13 21:57:28 +0500

    functions

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -1,82 +1,55 @@
 
+
+// Boss
 void main()
 {
-  // Write operation
-  List<String> friendList = [
-    "Ali",
-    "Bilal",
-    "Aftab",
-  ];
-
-  friendList.add("Zia");
-  friendList.insert(1, "Gia");
-
-  friendList[0] = "Ali Khan"; // update
-  friendList.add('Abid');
-  friendList.add("Imran Khan");
-
-  // Read - traversing - iterable
-  // for( var i = 0; i < friendList.length; i++ ){
-  //   print(friendList[i]);
-  //
-  // }
-
-  //print("*******************************");
-
-  // for in loop
-  // Traversing
-  // visiting the record
-  // for( String dost in friendList){
-  //   print(dost);
-  // }
-
-
-  // List<String> shoppingList = [
-  //   "Bread",
-  //   "Milk",
-  //   "Eggs",
-  //   "Pampers",
-  // ];
-  //
-  // for( var item in shoppingList){
-  //   print(item);
-  // }
-
-  Map<String, int> playerJersey = {
-    "afridi": 10,
-    "kohli": 17,
-    "babar": 56,
-    "ronaldo": 7,
-  };
-
-  //print(playerJersey['babar']);
-
-  // APIs - Web Services
-  Map<String, dynamic> user = {
-    "username": "abidkhan",
-    "dob": "1st Nov",
-    "age": 54,
-    "cgpa": 3.3,
-    "isMarried": true,
-    "friends": [ "Ali", "Bilal", "Khan"],
-  };
-
-  //print(user['isMarried']);
-
-  for( var key in user.keys){
-    print('$key = ${user[key]}');
-  }
-
-  for( var value in user.values){
-    print(value);
-  }
-
-  print('************************');
-  for( var entry in user.entries){
-    print('${entry.key} = ${entry.value}');
-  }
+  doubleIt(6);
+  sum(5,15); // actual arguments
+  sum(83, 7);
 }
 
+
+// function definition
+void displayStars( ) // header
+{
+  print('*****************************');
+}
+
+void peshawar() {
+  print("Chapli Kabab");
+}
+
+void karachi() {
+  print('Biryani');
+  lahore();
+  print('Back to Karachi');
+  kabul();
+
+}
+
+void lahore() {
+  print('Nihari');
+}
+
+void quetta() {
+  print('Sajji');
+}
+
+
+void kabul() {
+  print('Kabli Pulao');
+  print('Marray');
+}
+
+// positional parameters
+void sum( int a, int b )
+{
+  print("Addition = ${a +b }");
+}
+
+void doubleIt( int value ){
+  print('Double of $value is ${value*2}');
+}
 
 
 
