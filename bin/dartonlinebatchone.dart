b40bb3625135f03d7f0d570eb9e2266849253a4d
Abid <abidroid1@gmail.com>
2025-05-14 21:57:35 +0500

    fat arrow and named parameters

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -1,55 +1,35 @@
 
 
+// Named Parameters
+void table({ required int number, required int range })
+{
+  for( var i = 1; i <= range; i++ ){
+    print('$number X $i = ${number * i}');
+  }
+}
+
+// default value
+void product({required int n1, required int n2, int n3 = 1})
+{
+  print('Product = ${n1*n2*n3}');
+}
+
+
 // Boss
 void main()
 {
-  doubleIt(6);
-  sum(5,15); // actual arguments
-  sum(83, 7);
+  product(n1: 4, n2: 5, n3: 2);
+  product(n1: 5, n2: 5);
 }
-
 
 // function definition
-void displayStars( ) // header
-{
-  print('*****************************');
-}
-
-void peshawar() {
-  print("Chapli Kabab");
-}
-
-void karachi() {
-  print('Biryani');
-  lahore();
-  print('Back to Karachi');
-  kabul();
-
-}
-
-void lahore() {
-  print('Nihari');
-}
-
-void quetta() {
-  print('Sajji');
-}
-
-
-void kabul() {
-  print('Kabli Pulao');
-  print('Marray');
-}
+void displayStars() => print('*****************************');
 
 // positional parameters
-void sum( int a, int b )
-{
-  print("Addition = ${a +b }");
-}
+void sum( int a, int b ) => print("Addition = ${a +b }");
 
-void doubleIt( int value ){
-  print('Double of $value is ${value*2}');
-}
+void doubleIt( int value ) => print('Double of $value is ${value*2}');
+
 
 
 
