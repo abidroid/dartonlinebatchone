351ba72aa42413c86ed460137706fb617f8945fe
Abid <abidroid1@gmail.com>
2025-05-19 21:38:15 +0500

    return type

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -1,42 +1,18 @@
 
-
-// Named Parameters
-void table({ required int number, required int range })
-{
-  for( var i = 1; i <= range; i++ ){
-    print('$number X $i = ${number * i}');
-  }
+int square( int x ){
+  return x*x;
 }
-
-// default value
-void product({required int n1, required int n2, int n3 = 1})
-{
-  print('Product = ${n1*n2*n3}');
-}
-
 
 // Boss
-void main()
-{
-  product(n1: 4, n2: 5, n3: 2);
-  product(n1: 5, n2: 5);
+void main(){
+
+  print( square( square(  square(2) ) ) );
+
+
+  //
+  // int radius = 5;
+  // int result = square(radius);
+  //
+  // double areaOfCircle = 3.14 * result;
+  // print(areaOfCircle);
 }
-
-// function definition
-void displayStars() => print('*****************************');
-
-// positional parameters
-void sum( int a, int b ) => print("Addition = ${a +b }");
-
-void doubleIt( int value ) => print('Double of $value is ${value*2}');
-
-
-
-
-
-
-
-
-
-
-
