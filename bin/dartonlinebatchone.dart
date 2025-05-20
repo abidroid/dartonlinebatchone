30a934904be3335dbe76bc5bcea6f077a039d4eb
Abid <abidroid1@gmail.com>
2025-05-20 22:04:25 +0500

    class and objects

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -1,18 +1,43 @@
+class Student
+{
+  // data members
+  late String name;
+  late String mobile;
+  late int totalFee;
+  late int feePaid;
+  String? uni;
 
-int square( int x ){
-  return x*x;
+  // method
+  void show()
+  {
+    print("Name : $name");
+    print("Mob  : $mobile");
+    print("TFee : $totalFee");
+    print("FPaid: $feePaid");
+    print("Uni  : $uni");
+  }
 }
 
-// Boss
 void main(){
 
-  print( square( square(  square(2) ) ) );
+  Student ali;
+  ali = Student(); // default constructor
 
+  ali.name = "Muhammad Ali";
+  ali.mobile = "0838838383";
+  ali.totalFee = 10000;
+  ali.feePaid = 7000;
+  ali.uni = "SUIT";
 
-  //
-  // int radius = 5;
-  // int result = square(radius);
-  //
-  // double areaOfCircle = 3.14 * result;
-  // print(areaOfCircle);
+  ali.show();
+  print("******************");
+
+  Student hina;
+  hina = Student();
+  hina.name = "Hina Baji";
+  hina.mobile = "0383838383";
+  hina.totalFee = 10000;
+  hina.feePaid = 0;
+
+  hina.show();
 }
