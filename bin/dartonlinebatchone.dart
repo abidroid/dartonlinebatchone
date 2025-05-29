e2f0a90e700e407f6731367d455f78ef71182875
Abid <abidroid1@gmail.com>
2025-05-29 22:02:00 +0500

    polymorphism

--- bin/dartonlinebatchone.dart
+++ bin/dartonlinebatchone.dart
@@ -1,154 +1,58 @@
-// General Type
-class Person {
-  // data members
-  late String name;
-  late String gender;
-  late String dob;
-
-  // constructor
-  // parameterized constructor with named parameters
-  Person({required this.name, required this.gender, required this.dob});
-
-  // method
-  void display() {
-    print("Name:   $name");
-    print("Gender: $gender");
-    print("DOB:    $dob");
-  }
+abstract class Animal
+{
+  void eat() => print("Eating...");
+  void sleep() => print("Sleeping...");
+  // abstract function
+  void sound();
 }
 
-// Specialized Type
-// Beggar is a Person
-class Beggar extends Person {
-  // data member
-  late int dailyIncome;
-  late Address address; // has an Address
-
-  // constructor
-  Beggar({
-    required super.name,
-    required super.gender,
-    required super.dob,
-    required this.dailyIncome,
-  });
-
-  // method
-
+class Cat extends Animal
+{
   @override
-  void display() {
-    // method overriding
-    super.display();
-    print("Income: $dailyIncome");
+  void sound(){
+    print('Meow Meow Meow');
   }
 }
 
-class Address {
-  late String houseNum;
-  late String streetNum;
-  late String city;
-  late String country;
-
-  Address({
-    required this.houseNum,
-    required this.streetNum,
-    required this.city,
-    required this.country,
-  });
-
-  void showAddress() {
-    print('H# : $houseNum');
-    print('St#: $streetNum');
-    print('City: $city');
-    print('Country: $country');
+class Horse extends Animal
+{
+  @override
+  void sound(){
+    print("heheheheheh");
   }
 }
 
-void main() {
-  Doctor zia = Doctor(
-    name: 'Zia',
-    gender: 'M',
-    dob: "2 Jun",
-    spe: 'ENT',
-    fee: 2000,
-    address: Address(
-      houseNum: '2',
-      streetNum: '4',
-      city: "Kabul",
-      country: 'Afghanistan',
-    ),
-  );
+// pabandi - contract
+class Lion extends Animal
+{
+  @override
+  void sound() {
+    print('Roaring...');
+  }
 
-  // Address address1 = Address(
-  //   houseNum: '1',
-  //   streetNum: '2',
-  //   city: 'PSH',
-  //   country: 'Pakistan',
-  // );
-  // address1.showAddress();
-  //
-  // Beggar shehbaz = Beggar(
-  //   // constructor
-  //   name: "SS",
-  //   gender: "M",
-  //   dob: '2nd Jun',
-  //   dailyIncome: 5000,
-  // );
-  //
-  // shehbaz.dailyIncome = 15000; // update
-  // shehbaz.display();
-
-  // List<Beggar> mnas = [
-  //   shehbaz,
-  //   Beggar(name: "ABC", gender: 'M', dob: '2 Jun', dailyIncome: 4000),
-  //   Beggar(name: 'XYZ', gender: 'F', dob: '3 May', dailyIncome: 6000),
-  //   Beggar(name: 'AAA', gender: 'M', dob: '7 Sep', dailyIncome: 7000),
-  //
-  // ];
-  //
-  //
-  //
-  //
-  // Doctor ali = Doctor(
-  //   name: "Ali",
-  //   gender: "M",
-  //   dob: "2nd May",
-  //   spe: 'ENT',
-  //   fee: 2000,
-  // );
-  //
-  //
-  // List<Doctor> dakuList = [
-  //   ali,
-  //   Doctor(name: "Hina", gender: "F", dob: "2 March", spe: "Cardio", fee: 2500),
-  //   Doctor(name: 'Khan', gender: 'M', dob: '2 Jun', spe: 'Gastro', fee: 3500),
-  // ];
-  //
-  // for( var daku in dakuList){
-  //   daku.displayDoctor();
-  //   print("************************");
-  // }
 }
 
-// Specialized Type
-// Doctor is a Person
-class Doctor extends Person {
-  // data members
-  late String spe;
-  late int fee;
-  late Address address; // has an Address
 
-  // constructor
-  Doctor({
-    required super.name,
-    required super.gender,
-    required super.dob,
-    required this.spe,
-    required this.fee,
-    required this.address,
-  });
+void main(){
 
-  void displayDoctor() {
-    print("SPE:  $spe");
-    print("Fee:  $fee");
+  List<Animal> animals = [
+    Cat(),  // object instantiation
+    Horse(), // object instantiation
+    Lion(), // object instantiation
+    Cat(), // object instantiation
+  ];
+
+  for( var animal in animals){
+    animal.sound();
+    print("****************");
   }
+
+  // Polymorphism
+  // Animal animal;
+  // animal = Cat();
+  // animal.sound();
+  // animal = Horse();
+  // animal.sound();
+  // animal = Lion();
+  // animal.sound();
 }
